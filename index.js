0a4cc36930c4a7837733bc3c9b09884f91073af8
cenfun <cenfun@gmail.com>
2020-07-21 15:29:11 +0800

    init

--- index.js
+++ index.js
@@ -2,22 +2,6 @@ const os = require("os");
 const fs = require("fs");
 const path = require("path");
 
-const statsJson = require("./stats.json");
-
-const getEOL = function(content) {
-    if (!content) {
-        return os.EOL;
-    }
-    const nIndex = content.lastIndexOf("\n");
-    if (nIndex === -1) {
-        return os.EOL;
-    }
-    if (content.substr(nIndex - 1, 1) === "\r") {
-        return "\r\n";
-    }
-    return "\n";
-};
-
 const readFileContentSync = function(filePath) {
     let content = null;
     const isExists = fs.existsSync(filePath);
@@ -30,27 +14,55 @@ const readFileContentSync = function(filePath) {
     return content;
 };
 
-const writeFileContentSync = function(filePath, content, force) {
-    const isExists = fs.existsSync(filePath);
-    if (force || isExists) {
-        fs.writeFileSync(filePath, content);
-        return true;
-    }
-    return false;
-};
-
 const getDeps = function() {
-    return ["node_modules/turbogrid/dist/turbogrid.js"].map(item => {
-        const str = readFileContentSync(item);
-        if (str) {
-            //str = str.replace("//# sourceMappingURL=turbogrid.min.js.map", "");
+    return ["turbogrid/dist/turbogrid.js"].map(item => {
+        let p = path.resolve(__dirname, "./node_modules/", item);
+        if (!fs.existsSync(p)) {
+            p = path.resolve(__dirname, "../", item);
         }
+        const str = readFileContentSync(item) || "";
         return str;
-    }).join("\n");
+    }).join(os.EOL);
+};
+
+const getStatsJson = (option) => {
+    if (option.statsJson) {
+        return option.statsJson;
+    }
+
+    if (fs.existsSync(option.statsPath)) {
+        return require(option.statsPath);
+    }
 };
 
 
-const main = () => {
+const generateStatsReport = (option) => {
+
+    const defaultOption = {
+        statsPath: "stats.json",
+        statsJson: null,
+        outputPath: "./.temp/",
+        outputFilename: "stats-report.html"
+    };
+    option = Object.assign(defaultOption, option);
+
+    //create dir
+    if (!fs.existsSync(option.outputPath)) {
+        fs.mkdirSync(option.outputPath, {
+            recursive: true
+        });
+    }
+    
+    if (path.extname(option.outputFilename) !== ".html") {
+        option.outputFilename += ".html";
+    }
+
+    const statsJson = getStatsJson(option);
+    if (!statsJson) {
+        console.log("Not found stats, please pass statsJson or statsPath");
+        return;
+    }
+
     const modules = statsJson.modules.map(m => {
         let issuerPath = [];
         if (m.issuerPath) {
@@ -67,8 +79,6 @@ const main = () => {
             issuerPath: issuerPath
         };
     });
-
-    fs.writeFileSync("./modules.json", JSON.stringify(modules, null, 4));
     
     const tempPath = path.resolve(__dirname, "./template.html");
     let html = readFileContentSync(tempPath);
@@ -82,14 +92,14 @@ const main = () => {
         modules: modules
     };
 
-    jsStr += `\nthis.statsData = ${JSON.stringify(statsData)};`;
+    jsStr += `${os.EOL}this.statsData = ${JSON.stringify(statsData)};`;
 
     /*inject:start*/
     /*inject:end*/
     const scriptBlock = /(([ \t]*)\/\*\s*inject:start\s*\*\/)(\r|\n|.)*?(\/\*\s*inject:end\s*\*\/)/gi;
     const hasScriptBlock = scriptBlock.test(html);
     if (hasScriptBlock) {
-        const EOL = getEOL();
+        const EOL = os.EOL;
         html = html.replace(scriptBlock, function(match) {
             const list = [arguments[1]].concat(jsStr).concat(arguments[4]);
             const str = list.join(EOL + arguments[2]);
@@ -97,9 +107,9 @@ const main = () => {
         });
     }
 
-    const htmlPath = "stats-report.html";
-    writeFileContentSync(htmlPath, html, true);
+    const htmlPath = path.resolve(option.outputPath, option.outputFilename);
+    fs.writeFileSync(htmlPath, html);
 
 };
 
-main();
+export default generateStatsReport;
