da8c9558fa95eb997c0abd672b7eadc2c2fba0d9
cenfun <cenfun@gmail.com>
2020-10-16 14:38:35 +0800

    update for webpack 5

--- webpack.config.js
+++ webpack.config.js
@@ -29,7 +29,8 @@ module.exports = {
                     output: {
                         comments: false
                     }
-                }
+                },
+                extractComments: false
             })
         ]
     },
